c413629cd66262224894584ea591e295d396e85e
Marco <maurelima@gmail.com>
2021-07-29 14:33:28 -0300

    Jarvis

--- ExecServices.js
+++ ExecServices.js
@@ -11,90 +11,89 @@ const Store = require('./Store')
 const notifier = require('node-notifier');
 let crons = {}
 
-class ExecServices{
+class ExecServices {
 
-    constructor(options){
+    constructor(options) {
         const userDataPath = (electron.app || electron.remote.app).getPath('userData')
 
         this.path = path.join(userDataPath, 'services.json')
         if (fs.existsSync(this.path)) {
             this.data = parseDataFile(this.path)
-        }   
+        }
     }
 
-    updateData(){
+    updateData() {
         const storeServices = new StoreServices();
         this.data = storeServices.get()
-        //console.log(this.data)
     }
 
-    get(){
+    get() {
         return this.data
     }
 
-    servicesTask(){        
-       
-        try{ 
+    servicesTask() {
 
-            let data = this.get()   
-            
-            if(data != null && data != 'undefined'){
+        try {
 
-            let jobsToDoList = []
+            let data = this.get()
 
-            for(let i = 0; i < data.length; i++){
+            if (data != null && data != 'undefined') {
 
-            var name = data[i]['serviceName'];
-            var interv = data[i]['intTime'];
-            var active = data[i]['active'];
-            var id = data[i]['_id'];
+                let jobsToDoList = []
 
-            jobsToDoList.push({
-                id: id,
-                name: name,
-                active: active,
-                cron: '*/'+interv+' * * * * *'
-            })
+                for (let i = 0; i < data.length; i++) {
 
-            jobsToDoList.forEach( (job, idx) => {
-                crons[idx] = new CronJob(job.cron, () => {
-    
-                    if(job.active){
-                        exec('sc query '+job.name, (err, stdout, stderr) => {
-                            if (err) {  }    
-                            if(stdout.indexOf("RUNNING") != -1 || stdout.indexOf("RUNNING") != -1){
-                                //console.log('o serviço ' +'[' +job.name + ']' + ' está rodando')   
-                            }else{
-                                exec('sc start '+job.name, (err, stdout, stderr) => {
-                                    if (err) { }  
-                                    console.log('o serviço ' +'[' +job.name + ']' + ' está sendo iniciado') 
-                                     if(stdout.indexOf("FALHA") != -1 || stdout.indexOf("ERROR") != -1){
-                                         //console.log('Não foi possível iniciar o serviço: '+'[' +job.name + ']')  
-                                         this.setLog(stdout)
-                                     }else if(stdout.indexOf("RUNNING") != -1 || stdout.indexOf("RUNNING") != -1){
-                                        //console.log('o serviço ' +'[' +job.name + ']' + ' está rodando')  
+                    var name = data[i]['serviceName'];
+                    var interv = data[i]['intTime'];
+                    var active = data[i]['active'];
+                    var id = data[i]['_id'];
+
+                    jobsToDoList.push({
+                        id: id,
+                        name: name,
+                        active: active,
+                        cron: '*/' + interv + ' * * * * *'
+                    })
+
+                    jobsToDoList.forEach((job, idx) => {
+                        crons[idx] = new CronJob(job.cron, () => {
+
+                            if (job.active) {
+                                exec('sc query ' + job.name, (err, stdout, stderr) => {
+                                    if (err) { }
+                                    if (stdout.indexOf("RUNNING") != -1 || stdout.indexOf("RUNNING") != -1) {
+                                        //console.log('o serviço ' +'[' +job.name + ']' + ' está rodando')   
+                                    } else {
+                                        exec('sc start ' + job.name, (err, stdout, stderr) => {
+                                            if (err) { }
+                                            console.log('o serviço ' + '[' + job.name + ']' + ' está sendo iniciado')
+                                            if (stdout.indexOf("FALHA") != -1 || stdout.indexOf("ERROR") != -1) {
+                                                //console.log('Não foi possível iniciar o serviço: '+'[' +job.name + ']')  
+                                                this.setLog(stdout)
+                                            } else if (stdout.indexOf("RUNNING") != -1 || stdout.indexOf("RUNNING") != -1) {
+                                                //console.log('o serviço ' +'[' +job.name + ']' + ' está rodando')  
+                                            }
+                                        });
                                     }
-                                });
-                            }                            
 
-                        });
-                    } 
-    
-                }, null, true, 'America/Sao_Paulo')
-            })
+                                });
+                            }
+
+                        }, null, true, 'America/Sao_Paulo')
+                    })
+
+                }
 
             }
 
-        }
-
-        }catch(err){
-             this.setLog(err)
+        } catch (err) {
+            this.setLog(err)
         }
     }
 
-    setLog(err){
+    setLog(err) {
         let configs = this.getLog()
-        if(this.runNotify(configs['alertFrequency'], 'notfytime.json')){ 
+        if (this.runNotify(configs['alertFrequency'], 'notfytime.json')) {
             notifier.notify({
                 title: 'Falha ao executar Serviço',
                 message: `Erro: ${err}%`,
@@ -102,88 +101,82 @@ class ExecServices{
             });
         }
 
-        if(configs['makeLog']){
-            if(this.runNotify(configs['alertFrequency'], 'logtime.json')){
-                if(err.stack != 'undefined'){
-                    log.warn(err+"_break_")
-                }else{                    
-                    let logs = err.stack + "_break_"; 
+        if (configs['makeLog']) {
+            if (this.runNotify(configs['alertFrequency'], 'logtime.json')) {
+                if (err.stack != 'undefined') {
+                    log.warn(err + "_break_")
+                } else {
+                    let logs = err.stack + "_break_";
                     log.warn(logs);
                 }
-                               
-                
-                // if(configs['sendLog']){
-                //     //envia para o email
-                //     sendLogMail(String(logs))
-                // }
-            } 
-        } 
+
+            }
+        }
     }
 
-    stopServices(serviceName){
+    stopServices(serviceName) {
 
         for (let prop in crons) {
-            crons[prop].stop()            
+            crons[prop].stop()
         }
         crons = {}
 
-        exec('sc stop '+serviceName, (err, stdout, stderr) => {
-            if (err) { }   
+        exec('sc stop ' + serviceName, (err, stdout, stderr) => {
+            if (err) { }
         });
 
-        this.updateData() 
+        this.updateData()
 
     }
 
 
-    getLog(){
+    getLog() {
         let store = new Store({
             configName: 'user-settings',
             defaults: {
-              cpusettings: {
-                cpuOverload: 80,
-                alertFrequency: 5,
-                cpuEmail: 'mariocelso@customdata.com.br',
-                sendEmail: 1
-              },
-              logsettings:{
-                logEmail: 'mariocelso@customdata.com.br',
-                alertFrequency: '10',
-                makeLog: 1,
-                sendLog: 1
-              }
+                cpusettings: {
+                    cpuOverload: 80,
+                    alertFrequency: 5,
+                    cpuEmail: 'mariocelso@customdata.com.br',
+                    sendEmail: 1
+                },
+                logsettings: {
+                    logEmail: 'mariocelso@customdata.com.br',
+                    alertFrequency: '10',
+                    makeLog: 1,
+                    sendLog: 1
+                }
             }
-          });
-    
+        });
+
         return store.get('logsettings')
     }
 
 
-     runNotify(frequency, file) {
-          let storelog = new StoreLog(file);
-          if(storelog.get() == null || storelog.get() == 'undefined'){
+    runNotify(frequency, file) {
+        let storelog = new StoreLog(file);
+        if (storelog.get() == null || storelog.get() == 'undefined') {
             storelog.set(+new Date())
             return true
-          }
-          const notifyTime = new Date(parseInt(storelog.get()))
-          const now = new Date()
-          const diffTime = Math.abs(now - notifyTime)
-          const minutesPassed = Math.ceil(diffTime / (1000 * 60))
-          if(minutesPassed > frequency){
-              storelog.set(+new Date())
-              return true
-          }else{
-              false
-          }
-     }
+        }
+        const notifyTime = new Date(parseInt(storelog.get()))
+        const now = new Date()
+        const diffTime = Math.abs(now - notifyTime)
+        const minutesPassed = Math.ceil(diffTime / (1000 * 60))
+        if (minutesPassed > frequency) {
+            storelog.set(+new Date())
+            return true
+        } else {
+            false
+        }
+    }
 
 }
 
-
-function parseDataFile(filePath, defaults){
-    try{
+function parseDataFile(filePath, defaults) {
+    try {
         return JSON.parse(fs.readFileSync(filePath))
-    }catch(err){
+    } catch (err) {
         return defaults
     }
 }
--- ExecTask.js
+++ ExecTask.js
@@ -15,149 +15,135 @@ let crons = {}
 
 
 
-class ExecTask{
+class ExecTask {
 
-    constructor(options){
+    constructor(options) {
         const userDataPath = (electron.app || electron.remote.app).getPath('userData')
 
         this.path = path.join(userDataPath, 'tasks.json')
         if (fs.existsSync(this.path)) {
             this.data = parseDataFile(this.path)
-        }   
+        }
     }
 
-    
 
-    updateData(){
-            const storetasks = new StoreTasks();
-            this.data = storetasks.get()
+    updateData() {
+        const storetasks = new StoreTasks();
+        this.data = storetasks.get()
     }
 
-    get(){
+    get() {
         return this.data
     }
 
-    schedTask(){
+    schedTask() {
 
-        try{ 
-            
-        let data = this.get()   
-        
-        if(data != null && data != 'undefined'){        
+        try {
 
-        let jobsToDoList = []
+            let data = this.get()
 
-          for(let i = 0; i < data.length; i++){  
-                                       
-            var active = data[i]['active'];
-            var id = data[i]['_id'];
-            var start = data[i]['start'];
-            var end = data[i]['end'];
-            var interv = data[i]['intTime'];
-            var envpath = data[i]['envpath'];
-            var onlyopen = data[i]['onlyopen']
-            var proc = data[i]['proc']
-            var pathfile = data[i]['pathfile'];
+            if (data != null && data != 'undefined') {
+
+                let jobsToDoList = []
+
+                for (let i = 0; i < data.length; i++) {
+
+                    var active = data[i]['active'];
+                    var id = data[i]['_id'];
+                    var start = data[i]['start'];
+                    var end = data[i]['end'];
+                    var interv = data[i]['intTime'];
+                    var envpath = data[i]['envpath'];
+                    var onlyopen = data[i]['onlyopen']
+                    var proc = data[i]['proc']
+                    var pathfile = data[i]['pathfile'];
 
 
-            jobsToDoList.push({
-                //params: [id, proc, active, onlyopen],
-                id: id,
-                proc: proc,
-                active: active,
-                onlyopen: onlyopen,
-                start: start,
-                end: end,
-                pathfile: pathfile,
-                envpath: envpath,
-                cron: '*/'+interv+' * * * * *'
-            })
+                    jobsToDoList.push({
+                        id: id,
+                        proc: proc,
+                        active: active,
+                        onlyopen: onlyopen,
+                        start: start,
+                        end: end,
+                        pathfile: pathfile,
+                        envpath: envpath,
+                        cron: '*/' + interv + ' * * * * *'
+                    })
 
-            
-    
-        }         
 
-        jobsToDoList.forEach( (job, idx) => {
-            crons[idx] = new CronJob(job.cron, () => {
 
-                if(job.active){
+                }
 
-                    if(job.onlyopen){
-                        if(inTime(job.start, job.end)){
-                            exec('TASKLIST /FI "IMAGENAME eq '+job.proc+'"', (err, stdout, stderr) => {
-                                if (err) {
-                                    this.setLog(err)  
-                                }                          
-                                //se não encontrar tarefa, à abre (-1 significa que encontrou / diferente de -1 não encontrou)
-                                if(stdout.indexOf("nenhuma tarefa") != -1 || stdout.indexOf("No tasks") != -1){
-                                    let opts={  env: { PATH: job.envpath+process.env.path } }
-                                    //spawn('cmd.exe', ['/C', 'start /B '+job.pathfile], opts); 
-                                    exec('cmd.exe /C start /B '+job.pathfile, opts, (err, stdout, stderr) => {
-                                        if(err){this.setLog(err)}
-                                        //console.log(stdout)
-                                    })
-                                    //console.log(job.proc+ ' iniciado')
-                
-                                }else{
-                                    //console.log(job.proc+' já está em execução')
+                jobsToDoList.forEach((job, idx) => {
+                    crons[idx] = new CronJob(job.cron, () => {
+
+                        if (job.active) {
+
+                            if (job.onlyopen) {
+                                if (inTime(job.start, job.end)) {
+                                    exec('TASKLIST /FI "IMAGENAME eq ' + job.proc + '"', (err, stdout, stderr) => {
+                                        if (err) {
+                                            this.setLog(err)
+                                        }
+                                        if (stdout.indexOf("nenhuma tarefa") != -1 || stdout.indexOf("No tasks") != -1) {
+                                            let opts = { env: { PATH: job.envpath + process.env.path } }
+                                            //spawn('cmd.exe', ['/C', 'start /B '+job.pathfile], opts); 
+                                            exec('cmd.exe /C start /B ' + job.pathfile, opts, (err, stdout, stderr) => {
+                                                if (err) { this.setLog(err) }
+                                            })
+                                        }
+                                    });
                                 }
-                            });
-                        }
-                    }else{
-                        if(inTime(job.start, job.end)){
-                            exec('TASKLIST /FI "IMAGENAME eq '+job.proc+'"', (err, stdout, stderr) => {
-                                if (err) {
-                                    this.setLog(err)  
-                                }                          
-                                //se não encontrar tarefa, à abre (-1 significa que encontrou / diferente de -1 não encontrou)
-                                if(stdout.indexOf("nenhuma tarefa") != -1 || stdout.indexOf("No tasks") != -1){
-                                    let opts={  env: { PATH: job.envpath+process.env.path } }
-                                    //spawn('cmd.exe', ['/C', 'start /B '+job.pathfile], opts); 
-                                    exec('cmd.exe /C start /B '+job.pathfile, opts, (err, stdout, stderr) => {
-                                        if(err){this.setLog(err)}
-                                        console.log(stdout)
-                                    })
-                                    //console.log(job.proc+ ' iniciado')
-                
-                                }else{
-                                    //console.log(job.proc+' já está em execução')
-                                }
-                            });
-                        }else{
-                            if(ofTime(job.end)){
-                                exec('tasklist /FI "ImageName eq '+job.proc+'" /FI "Status eq Running" /FO LIST', (err, stdout, stderr) => {
-                                    if (err) {
-                                        this.setLog(err)  
-                                    } 
-                                    //se encontrar tarefa, fecha ela (-1 significa que encontrou / diferente de -1 não encontrou)
-                                    if(stdout.indexOf("nenhuma tarefa") != -1 || stdout.indexOf("No tasks") != -1){                                                          
-                                        //console.log(job.proc+ 'está fechado')
-                                    }else{                      
-                                        exec('taskkill /F /IM '+job.proc, (err, stdout, stderr) => {
-                                            console.log(stdout)
+                            } else {
+                                if (inTime(job.start, job.end)) {
+                                    exec('TASKLIST /FI "IMAGENAME eq ' + job.proc + '"', (err, stdout, stderr) => {
+                                        if (err) {
+                                            this.setLog(err)
+                                        }
+                                        if (stdout.indexOf("nenhuma tarefa") != -1 || stdout.indexOf("No tasks") != -1) {
+                                            let opts = { env: { PATH: job.envpath + process.env.path } }
+                                            //spawn('cmd.exe', ['/C', 'start /B '+job.pathfile], opts); 
+                                            exec('cmd.exe /C start /B ' + job.pathfile, opts, (err, stdout, stderr) => {
+                                                if (err) { this.setLog(err) }
+                                                console.log(stdout)
+                                            })
+
+                                        }
+                                    });
+                                } else {
+                                    if (ofTime(job.end)) {
+                                        exec('tasklist /FI "ImageName eq ' + job.proc + '" /FI "Status eq Running" /FO LIST', (err, stdout, stderr) => {
+                                            if (err) {
+                                                this.setLog(err)
+                                            }
+                                            if (stdout.indexOf("nenhuma tarefa") != -1 || stdout.indexOf("No tasks") != -1) {
+                                            } else {
+                                                exec('taskkill /F /IM ' + job.proc, (err, stdout, stderr) => {
+                                                    console.log(stdout)
+                                                });
+                                            }
                                         });
                                     }
-                                });                                
+                                }
                             }
+
                         }
-                    }
 
-                }   
+                    }, null, true, 'America/Sao_Paulo')
+                })
 
-            }, null, true, 'America/Sao_Paulo')
-        })
-        
+            }
+
+
+        } catch (err) {
+            this.setLog(err)
+        }
     }
 
-
-    }catch(err){
-        this.setLog(err)   
-    }
-    }
-
-    setLog(err){
+    setLog(err) {
         let configs = this.getLog()
-        if(this.runNotify(configs['alertFrequency'], 'notfytime.json')){ 
+        if (this.runNotify(configs['alertFrequency'], 'notfytime.json')) {
             notifier.notify({
                 title: 'Falha ao executar a Tarefa',
                 message: `Erro: ${err}%`,
@@ -165,119 +151,109 @@ class ExecTask{
             });
         }
 
-        if(configs['makeLog']){
-            if(this.runNotify(configs['alertFrequency'], 'logtime.json')){
+        if (configs['makeLog']) {
+            if (this.runNotify(configs['alertFrequency'], 'logtime.json')) {
                 console.log(err.stack)
-                if(err.stack != 'undefined'){
-                    log.warn(err+"_break_")
-                }else{                    
-                    let logs = err.stack + "_break_"; 
+                if (err.stack != 'undefined') {
+                    log.warn(err + "_break_")
+                } else {
+                    let logs = err.stack + "_break_";
                     log.warn(logs);
                 }
-                               
-                
-                // if(configs['sendLog']){
-                //     //envia para o email
-                //     sendLogMail(String(logs))
-                // }
-            } 
-        } 
+            }
+        }
     }
 
-    stopTasks(proc){
+    stopTasks(proc) {
 
         for (let prop in crons) {
-            crons[prop].stop()            
+            crons[prop].stop()
         }
         crons = {}
 
-        exec('taskkill /F /IM '+proc, (err, stdout, stderr) => {
+        exec('taskkill /F /IM ' + proc, (err, stdout, stderr) => {
             console.log(stdout)
         });
 
-        this.updateData()            
+        this.updateData()
 
     }
 
-
-    getLog(){
+    getLog() {
         let store = new Store({
             configName: 'user-settings',
             defaults: {
-              cpusettings: {
-                cpuOverload: 80,
-                alertFrequency: 5,
-                cpuEmail: 'mariocelso@customdata.com.br',
-                sendEmail: 1
-              },
-              logsettings:{
-                logEmail: 'mariocelso@customdata.com.br',
-                alertFrequency: '10',
-                makeLog: 1,
-                sendLog: 1
-              }
+                cpusettings: {
+                    cpuOverload: 80,
+                    alertFrequency: 5,
+                    cpuEmail: 'mariocelso@customdata.com.br',
+                    sendEmail: 1
+                },
+                logsettings: {
+                    logEmail: 'mariocelso@customdata.com.br',
+                    alertFrequency: '10',
+                    makeLog: 1,
+                    sendLog: 1
+                }
             }
-          });
-    
+        });
+
         return store.get('logsettings')
     }
 
-
-     runNotify(frequency, file) {
-          let storelog = new StoreLog(file);
-          if(storelog.get() == null || storelog.get() == 'undefined'){
+    runNotify(frequency, file) {
+        let storelog = new StoreLog(file);
+        if (storelog.get() == null || storelog.get() == 'undefined') {
             storelog.set(+new Date())
             return true
-          }
-          const notifyTime = new Date(parseInt(storelog.get()))
-          const now = new Date()
-          const diffTime = Math.abs(now - notifyTime)
-          const minutesPassed = Math.ceil(diffTime / (1000 * 60))
-          if(minutesPassed > frequency){
-              storelog.set(+new Date())
-              return true
-          }else{
-              false
-          }
-     }
-
- 
+        }
+        const notifyTime = new Date(parseInt(storelog.get()))
+        const now = new Date()
+        const diffTime = Math.abs(now - notifyTime)
+        const minutesPassed = Math.ceil(diffTime / (1000 * 60))
+        if (minutesPassed > frequency) {
+            storelog.set(+new Date())
+            return true
+        } else {
+            false
+        }
+    }
 
 }
 
-function inTime(start, end){
+function inTime(start, end) {
 
-    let datenow = new Date().toJSON().slice(0,10);             
-    let date1 = utcToZonedTime(new Date(datenow+' ' + start), timeZone)
-    let date2 = utcToZonedTime(new Date(datenow+' ' + end), timeZone)    
+    let datenow = new Date().toJSON().slice(0, 10);
+    let date1 = utcToZonedTime(new Date(datenow + ' ' + start), timeZone)
+    let date2 = utcToZonedTime(new Date(datenow + ' ' + end), timeZone)
 
-    let date3 = utcToZonedTime(new Date(), timeZone); 
-    if(isBefore(date1, date3) && isAfter(date2, date3)){ 
+    let date3 = utcToZonedTime(new Date(), timeZone);
+    if (isBefore(date1, date3) && isAfter(date2, date3)) {
         return true
-    }else{
+    } else {
         return false
     }
-    
+
 }
 
-function ofTime(end){
+function ofTime(end) {
 
-    let datenow = new Date().toJSON().slice(0,10);             
-    let date2 = utcToZonedTime(new Date(datenow+' ' + end), timeZone)    
+    let datenow = new Date().toJSON().slice(0, 10);
+    let date2 = utcToZonedTime(new Date(datenow + ' ' + end), timeZone)
 
-    let date3 = utcToZonedTime(new Date(), timeZone); 
-    if(isAfter(date3, date2) || isEqual(date3, date2)){  
+    let date3 = utcToZonedTime(new Date(), timeZone);
+    if (isAfter(date3, date2) || isEqual(date3, date2)) {
         return true
-    }else{
+    } else {
         return false
     }
-    
+
 }
 
-function parseDataFile(filePath, defaults){
-    try{
+function parseDataFile(filePath, defaults) {
+    try {
         return JSON.parse(fs.readFileSync(filePath))
-    }catch(err){
+    } catch (err) {
         return defaults
     }
 }
